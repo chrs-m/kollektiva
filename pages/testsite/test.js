06de5958d9aa08f2f3b1767f02f7438d5d5b91ea
chrs-m <mister.ch@gmail.com>
2022-03-21 18:22:54 +0100

    Install Supabase and dotenv

--- pages/testsite/test.js
+++ pages/testsite/test.js
@@ -1,5 +1,24 @@
+import "dotenv/config";
+import { createClient } from "@supabase/supabase-js";
 import styles from "./test.module.scss";
+
+const { NEXT_PUBLIC_SUPABASE_KEY, NEXT_PUBLIC_SUPABASE_URL } = process.env;
 
 export default function Home() {
   return <div className={styles.testDiv}></div>;
 }
+
+// Create a single supabase client for interacting with your database
+const supabaseConnect = createClient(
+  { NEXT_PUBLIC_SUPABASE_URL },
+  { NEXT_PUBLIC_SUPABASE_KEY }
+);
+
+// Provide a custom `fetch` implementation as an option
+const supabaseFetch = createClient(
+  { NEXT_PUBLIC_SUPABASE_URL },
+  { NEXT_PUBLIC_SUPABASE_KEY },
+  {
+    fetch: (...args) => fetch(...args),
+  }
+);
