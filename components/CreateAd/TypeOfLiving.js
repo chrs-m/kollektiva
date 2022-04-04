067854862e2c86950fb365e274254a0258806df2
amandahulten <amahul1029@skola.goteborg.se>
2022-04-04 15:46:38 +0200

    added component to buttons

--- components/CreateAd/TypeOfLiving.js
+++ components/CreateAd/TypeOfLiving.js
@@ -1,22 +1,88 @@
 import React from "react";
 import styles from "../../styles/AdStyling/TypeOfLiving.module.scss";
+import buttonsvg from "../../assets/images/button-img.svg";
+import plus from "../../assets/images/plus.svg";
+import ManyOptionsBtn from "../ManyOptionsBtn";
 
 function TypeOfLiving() {
+  // Får inte ut bilderna
+  //Flytta knappen lägg till text
+
+  const HorizontalLine = () => (
+    <hr style={{ backgroundColor: "#000", width: "100%" }} />
+  );
   return (
     <div className={styles.container}>
-      <label>Storlek på boende</label>
-      <input type="number" placeholder="Skriv i antal kvm"></input>
-      <h3>Klicka i de rutor som stämmer</h3>
-      {/* Göra om till radio-button? Så man bara har ett val och inte kan välja flera? */}
-      <div>
-        <input type="radio" id="house" name="living" value="house" />
-        <label htmlFor="house">Hel bostad</label>
-        <input type="radio" id="room" name="living" value="room" />
-        <label htmlFor="room">Rum i bostad</label>
-        <input type="radio" id="floor" name="living" value="floor" />
-        <label htmlFor="floor">Egen våning i bostad</label>
-        <input type="radio" id="shared" name="living" value="shared" />
-        <label htmlFor="shared">Bostad på delad tomt</label>
+      <label htmlFor="houseSize">Storlek på boende:</label> <br />
+      <input
+        type="number"
+        placeholder="Skriv i antal kvm"
+        id="houseSize"
+        className={styles.houseSize}
+      ></input>
+      <h3>Jag hyr ut:</h3>
+      <div className={styles.houseTypeContainer}>
+        <ManyOptionsBtn text="Hel bostad" />
+        <ManyOptionsBtn text="Rum i bostad" />
+        <ManyOptionsBtn text="Egen våning i bostad" />
+        <ManyOptionsBtn text="Bostad på delad tomt" />
+      </div>
+      <h3>Min bostad är:</h3>
+      <div className={styles.furnishedContainer}>
+        <ManyOptionsBtn text="Möblerad" />
+        <ManyOptionsBtn text="Omöblerad" />
+        <ManyOptionsBtn text="Delvis möblerad" />
+      </div>
+      <h3>Vill du addera eller förtydliga informationen ovan?</h3>
+      <div className={styles.addTextContainer}>
+        <textarea
+          className={styles.addText}
+          placeholder="Tex. Jag erbjuder rum i bostad med delad toalett och delat kök"
+        ></textarea>
+        <button className={styles.addTextBtn}>Lägg till text</button>
+      </div>
+      <h2>Vad erbjuder din bostad?</h2>
+      <h3>Höjdpunkter</h3>
+      <h4>Klicka i de rutor som stämmer:</h4>
+      <div className={styles.extrasContainer}>
+        <ManyOptionsBtn text="Centralt" />
+        <ManyOptionsBtn text="Naturnära" />
+        <ManyOptionsBtn text="Nära skola" />
+        <ManyOptionsBtn text="Nära affär" />
+        <ManyOptionsBtn text="Familjevänligt" />
+        <ManyOptionsBtn text="Nära kollektivtrafik" />
+      </div>
+      <h3>Lägg till egna:</h3>
+      <input
+        type="text"
+        placeholder="Expemelvis, nära hav"
+        className={styles.facilities}
+      >
+        {/* <button>
+            <img src={plus} />
+          </button> */}
+      </input>
+      <h3>Faciliteter</h3>
+      <h4>Klicka i de rutor som stämmer:</h4>
+      <div className={styles.extrasContainer}>
+        <ManyOptionsBtn text="Tvättmaskin" />
+        <ManyOptionsBtn text="Diskmaskin" />
+        <ManyOptionsBtn text="Torktumlare" />
+        <ManyOptionsBtn text="Wifi" />
+        <ManyOptionsBtn text="TV" />
+        <ManyOptionsBtn text="Balkong" />
+        <ManyOptionsBtn text="Parkering" />
+        <ManyOptionsBtn text="Trädgård" />
+        <ManyOptionsBtn text="Plats för cykel" />
+      </div>
+      <HorizontalLine />
+      <div className={styles.addFacilities}>
+        <h3>Lägg till egna faciliteter:</h3>
+        <input
+          type="text"
+          placeholder="Expemelvis, badkar"
+          className={styles.facilities}
+        ></input>
       </div>
     </div>
   );
