b16425c510800ac0c422a7c5c308b33b7785d076
chrs-m <mister.ch@gmail.com>
2022-04-05 06:55:35 +0200

    Add new font and some styling

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,70 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.scss";
 import Navbar from "../components/Navbar/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown, faHouseChimney } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Navbar />
-      <button>
-        <Link href="/login">
-          <a>Logga in här!</a>
-        </Link>
-      </button>
+      <div className={styles.HeaderImgContainer}>
+        <div className={styles.BackgroundImage}></div>
+        <h1 className={styles.Title}>
+          Välkommen till Kollektiva
+          <br />
+          <span>- En trygg andrahandsuthyrning</span>
+        </h1>
+      </div>
+      <div className={styles.BtnsContainer}>
+        <button className={styles.btn}>
+          <Link href="/login">
+            <a>Sök bostad</a>
+          </Link>
+        </button>
+        <button className={styles.btn}>
+          <Link href="/login">
+            <a>Hyr ut bostad</a>
+          </Link>
+        </button>
+      </div>
+      <FontAwesomeIcon icon={faArrowDown} className={styles.DownArrow} />
+
+      <div className={styles.MainSection}>
+        <div className={styles.InfoContainer}>
+          <FontAwesomeIcon
+            icon={faHouseChimney}
+            className={styles.HouseChimney}
+          />
+          <h2 className={styles.InfoHeader}>En trygg och säker plattform</h2>
+          <p className={styles.InfoPara}>
+            På Kollektiva görs regelbundna kontroller så att du, som
+            hyresvärd/hryesgäst skall känna dig trygg under hela processen.
+          </p>
+        </div>
+        <div className={styles.InfoContainer}>
+          <FontAwesomeIcon
+            icon={faHouseChimney}
+            className={styles.HouseChimney}
+          />
+          <h2 className={styles.InfoHeader}>En trygg och säker plattform</h2>
+          <p className={styles.InfoPara}>
+            På Kollektiva görs regelbundna kontroller så att du, som
+            hyresvärd/hryesgäst skall känna dig trygg under hela processen.
+          </p>
+        </div>
+        <div className={styles.InfoContainer}>
+          <FontAwesomeIcon
+            icon={faHouseChimney}
+            className={styles.HouseChimney}
+          />
+          <h2 className={styles.InfoHeader}>En trygg och säker plattform</h2>
+          <p className={styles.InfoPara}>
+            På Kollektiva görs regelbundna kontroller så att du, som
+            hyresvärd/hryesgäst skall känna dig trygg under hela processen.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
