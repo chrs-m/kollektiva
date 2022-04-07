f1d6f4998ab4d0a6a594f31e326ba1d4c4355671
chrs-m <mister.ch@gmail.com>
2022-04-07 14:55:37 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.experiments = { topLevelAwait: true };
-
-    config.node = {
-      fs: "empty",
-      net: "empty",
-      tls: "empty",
-    };
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
