353f661e0153c5f9ee525ee06f0fbfb7886a84d2
chrs-m <mister.ch@gmail.com>
2022-04-07 15:00:30 +0200

    Update test.js

--- pages/testsite/test.js
+++ pages/testsite/test.js
@@ -1,24 +1,24 @@
-import "dotenv/config";
-import { createClient } from "@supabase/supabase-js";
-import styles from "./test.module.scss";
+// import "dotenv/config";
+// import { createClient } from "@supabase/supabase-js";
+// import styles from "./test.module.scss";
 
-const { NEXT_PUBLIC_SUPABASE_KEY, NEXT_PUBLIC_SUPABASE_URL } = process.env;
+// const { NEXT_PUBLIC_SUPABASE_KEY, NEXT_PUBLIC_SUPABASE_URL } = process.env;
 
-export default function Home() {
-  return <div className={styles.testDiv}></div>;
-}
+// export default function Home() {
+//   return <div className={styles.testDiv}></div>;
+// }
 
-// Create a single supabase client for interacting with your database
-const supabaseConnect = createClient(
-  { NEXT_PUBLIC_SUPABASE_URL },
-  { NEXT_PUBLIC_SUPABASE_KEY }
-);
+// // Create a single supabase client for interacting with your database
+// const supabaseConnect = createClient(
+//   { NEXT_PUBLIC_SUPABASE_URL },
+//   { NEXT_PUBLIC_SUPABASE_KEY }
+// );
 
-// Provide a custom `fetch` implementation as an option
-const supabaseFetch = createClient(
-  { NEXT_PUBLIC_SUPABASE_URL },
-  { NEXT_PUBLIC_SUPABASE_KEY },
-  {
-    fetch: (...args) => fetch(...args),
-  }
-);
+// // Provide a custom `fetch` implementation as an option
+// const supabaseFetch = createClient(
+//   { NEXT_PUBLIC_SUPABASE_URL },
+//   { NEXT_PUBLIC_SUPABASE_KEY },
+//   {
+//     fetch: (...args) => fetch(...args),
+//   }
+// );
