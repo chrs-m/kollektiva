07bc3bba045242bfa11e725d357cae4afdd31040
chrs-m <mister.ch@gmail.com>
2022-04-11 18:18:25 +0200

    Style Extras page
    Some tweaks on tablet still needs to be done. Overall mostly done.
    
    Fixed height issue on login page.
    
    Corrected Textarea component.

--- components/CreateAd/Extras.js
+++ components/CreateAd/Extras.js
@@ -1,5 +1,8 @@
 import { React, useState } from "react";
 import styles from "../../styles/AdStyling/Extras.module.scss";
+import Textarea from "../Parts/Textarea";
+import Radio from "../Parts/Radio";
+import { BiImageAdd } from "react-icons/bi";
 
 function Extras({ formData, setFormData }) {
   const [toggle, setToggle] = useState(false);
@@ -10,44 +13,74 @@ function Extras({ formData, setFormData }) {
 
   return (
     <div className={styles.container}>
-      <label>Ge ett namn till ditt boende</label>
-      <input
-        type="text"
+      <label className={styles.LabelTitle}>Ge ett namn till ditt boende</label>
+
+      <Textarea
         placeholder="Förslag: Modernt familjevändligt hus med trädgård"
         value={formData.adName}
         onChange={(event) => {
           setFormData({ ...formData, adName: event.target.value });
         }}
-      ></input>
-      <label>Beskriv kortfattat ditt boende</label>
-      <textarea
+      />
+      <label className={styles.LabelTitle}>
+        Beskriv kortfattat ditt boende
+      </label>
+
+      <Textarea
         placeholder="Förslag: Rymligt boende med plats för hela familjen i mysiga Lerum."
         value={formData.adDescription}
         onChange={(event) => {
           setFormData({ ...formData, adDescription: event.target.value });
         }}
-      ></textarea>
-      <div className={styles.pictureContainer}>
+      />
+      <div className={styles.PictureContainer}>
         <h2>Ladda upp bilder</h2>
         <h3>
-          Ladda upp minst 5 bilder. Tänk på att ta tydliga bilder som beskriver
-          boendet väl.
+          Ladda upp <span>minst 5 bilder.</span> Tänk på att ta tydliga bilder
+          som beskriver boendet väl.
         </h3>
-        <form>
+        {/* <form>
           <input type="file" id="picture" name="filename" />
           <input type="submit" />
-        </form>
+        </form> */}
 
-        <img></img>
-        <img></img>
-        <img></img>
-        <img></img>
-        <h3>Har du inga foton ännu?</h3>
-        <input type="radio" id="no" name="pictures" value="no" />
-        <label htmlFor="no">Nej jag ska ta bilder</label>
-        <input type="radio" id="help" name="pictures" value="help" />
-        <label htmlFor="help">Nej, jag vill ha hjälp med det</label>
-        <small>Vi på Kollektiva kontaktar dig för handledning.</small>
+        <div className={styles.ImgWrapperLarge}>
+          <p>Klicka här för att ladda upp foto</p>
+          <BiImageAdd className={styles.AddImgSvgLarge} />
+        </div>
+        <div className={styles.ImgWrapperContainer}>
+          <div className={styles.ImgWrapper}>
+            <BiImageAdd className={styles.AddImgSvg} />
+          </div>
+          <div className={styles.ImgWrapper}>
+            <BiImageAdd className={styles.AddImgSvg} />
+          </div>
+          <div className={styles.ImgWrapper}>
+            <BiImageAdd className={styles.AddImgSvg} />
+          </div>
+          <div className={styles.ImgWrapper}>
+            <BiImageAdd className={styles.AddImgSvg} />
+          </div>
+        </div>
+
+        <h2>Har du inga foton ännu?</h2>
+        <Radio
+          type="radio"
+          id="no"
+          name="pictures"
+          value="no"
+          for="no"
+          text="Nej, jag skall ta bilder"
+        />
+        <Radio
+          type="radio"
+          id="help"
+          name="pictures"
+          value="help"
+          for="help"
+          text="Nej, jag vill ha hjälp med det."
+        />
+        <small>(Vi på Kollektiva kontaktar dig för handledning.)</small>
       </div>
     </div>
   );
